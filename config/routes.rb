dac1883555ec2e507c5cbc9b89e75b0fa0b0bf49
Victor Sc <victor.sc175@gmail.com>
2018-02-14 16:46:52 +0200

    Add Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/my_portfolio'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio' 
 end
