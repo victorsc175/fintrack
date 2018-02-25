f1871aaf68610e1a88818d7d03aab13cd5af5a51
Victor Sc <victor.sc175@gmail.com>
2018-02-25 14:52:34 +0200

    Routes cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,10 @@ Rails.application.routes.draw do
   resources :user_stocks, except: [:show, :edit, :update]
   resources :users, only: [:show]
   resources :friendships
-  get 'stocks/search'
-  get 'users/my_portfolio'
   get 'my_friends', to: 'users#my_friends'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio' 
   get 'search_stocks', to: 'stocks#search'
   get 'search_friends', to: 'users#search'
   post 'add_friend', to: 'users#add_friend'
+  root 'welcome#index'
 end
