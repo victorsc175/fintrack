c449aa0d8963330abdd25c06f2367938a7e01163
Victor Sc <victor.sc175@gmail.com>
2018-03-08 11:42:29 +0200

    Config deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,29 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+lock '~> 3.10.1'
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'fintrack'
+set :repo_url, 'git@github.com:victorsc175/fintrack.git'
+
+server '185.67.3.54', roles: %w(app db)
+set :ssh_options, user: 'victor'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set(:branch, ENV['BRANCH']) if ENV['BRANCH'] 
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/victor/www/fintrack"
+
+set :bundle_jobs, 4
+set :format, :pretty
+set :log_level, :debug
+set :pty, true
+append :linked_dirs, 'log', 'tmp/pids', 'sockets', 'public/static'
+set :rvm_type, :user
+set :rvm_ruby_version, '2.4.1'
+set :default_env, rails_env: fetch(:stage)
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -37,3 +52,4 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
